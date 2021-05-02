dff91513c08f6fa6459b9628420fc44b827fd8a6
homerjay <homerjay@naver.com>
2021-05-03 01:20:38 +0900

    animation, tween,Curves,controller

--- lib/src/flash_chat/welcome_screen.dart
+++ lib/src/flash_chat/welcome_screen.dart
@@ -15,6 +15,8 @@ class WelcomeScreen extends StatefulWidget {
 
 class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
   AnimationController controller;
+  Animation animation;
+  Animation animationTween;
 
   @override
   void initState() {
@@ -23,17 +25,38 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
     controller = AnimationController(
         duration: Duration(seconds: 1), //1초간 애니메이션 지속시간
         vsync: this,
-        upperBound: 100   //50으로 설정하면 컨트롤러의 밸류값이 50이되면 멈춤
+        // upperBound: 60   //50으로 설정하면 컨트롤러의 밸류값이 50이되면 멈춤
     );
     controller.forward(); //0~60까지의 티커를 가지게됨
+    // controller.reverse(from: 1); //60~0까지의 티커를 가지게됨 (반대로됨)
     controller.addListener(() {   //리스너 (결과)에다가
       setState(() {});            //setstate를 넘겨 실시간 반영이 되게끔만든다.
+      print(animation.value);
     });
+
+/*    controller.addStatusListener((status) {  //status로 애니메이션의 시작,끝을 알수있음
+      if(status == AnimationStatus.completed){    //끝나면
+        controller.reverse(from: 1);       //1까지 줄인다,
+      }else if(status == AnimationStatus.dismissed){ //해지되면
+        controller.forward(); //0~60까지의 티커를 실행
+      }
+    });*/
+    animation = CurvedAnimation(parent: controller, curve: Curves.elasticInOut); //parent는 적용대상할 애니메 컨트롤러
+
+    //컬러트윈 애니메이션 비긴,엔드를 지정하고 .animate로 컨트롤러를 지정해주어야함.
+    animationTween = ColorTween(begin: Colors.red,end: Colors.blue).animate(controller);
   }
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animationTween.value,
       body: SafeArea(
         child: Center(
           child: Padding(
@@ -50,12 +73,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
                       child: Container(
                         child: Image.asset(
                           "assets/images/logo.png",
+                          // height: animation.value * 90,
                           height: 60,
                         ),
                       ),
                     ),
                     Text(
-                      '${controller.value.toInt()}%',
+                      'Flash Chat',
                       style: TextStyle(
                           color: Colors.grey,
                           fontWeight: FontWeight.bold,
